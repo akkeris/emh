e0a073523dafec2d1d9cb6c5e452c90dbc583ed0
Murray Resinski <murray.resinski@octanner.com>
2019-07-02 20:55:33 -0600

    be a little more specific on what to expect from a list of artifacts

--- spec/features/taas/register_and_run_test_spec.rb
+++ spec/features/taas/register_and_run_test_spec.rb
@@ -135,6 +135,7 @@ feature 'register a test and run it', sauce: false do
      artifactsbody, artifactsstatus = app.taas.get_artifacts(successid)
      $stdout.puts artifactsbody, artifactsstatus
      expect(artifactsbody).not_to be_empty
+     expect(artifactsbody).to include ('href="ss.png"')
      expect(artifactsstatus).to eq 200     
 
  end
